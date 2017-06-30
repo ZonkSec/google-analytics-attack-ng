53315b92ac8cc2b0041df5e15348284428d0e66d
Tyler Rosonke <tyler@zonksec.com>
2017-06-29 23:00:00 -0500

    added jitter functionality

--- main.py
+++ main.py
@@ -4,25 +4,18 @@ import re
 import time
 import random
 import google
+import threading
 
 #https://developers.google.com/analytics/devguides/collection/protocol/v1/
 #https://developers.google.com/analytics/devguides/collection/protocol/v1/geoid
 
 def main():
-    #test = analytics_request(document_referrer="http://hackerzzz.com", document_location="https://zonksec.com/me",tracking_id='UA-72589501-1',client_id=559,geo_id='1007949')
-    #test.send()
 
-    # search_results = google.search(query='site:zonksec.com',num=10,stop=1)
-    # test =[]
-    # for result in search_results:
-    #     test.append(result)
-    # print test
+    test = single_page_attack(target_url='http://zonksec.com',referral_url='http://hacker.com',client_id=402,bounces=10,page_delay_jitter=.50,page_delay=30)
+    test.run()
 
-    test = single_page_attack(target_url='http://zonksec.com',referral_url='http://hacker.com',client_id=402,bounces=3)
-    print len(test.bounce_urls)
-    #test.run()
 class single_page_attack:
-    def __init__(self, target_url, client_id, referral_url, bounce_urls = None, page_delay=5, page_delay_jitter=10, bounces=0):
+    def __init__(self, target_url, client_id, referral_url, bounce_urls = None, page_delay=30, page_delay_jitter=.10, bounces=0, loop=1):
         self.target_url = target_url
         self.client_id = client_id
         self.referral_url = referral_url
@@ -30,6 +23,7 @@ class single_page_attack:
         self.page_delay_jitter = page_delay_jitter
         self.bounces = bounces
         self.bounce_urls = bounce_urls
+        self.loop=loop
 
         #if bounce urls are need, collects them.
         if self.bounces <= 10 and self.bounces != 0 and self.bounce_urls is None:
@@ -43,18 +37,33 @@ class single_page_attack:
             for result in search_results:
                 self.bounce_urls.append(str(result))
 
+    def run(self,client_id=None):
+        if client_id is not None:
+            self.client_id = client_id
+        pages = []
+        loop_count = 0
+        last_page = self.referral_url
+        while loop_count < self.loop:
+            target_request = analytics_request(document_location=self.target_url,document_referrer=last_page,client_id=self.client_id)
+            target_request.send()
+            pages.append(self.target_url)
+            bounce_count = 0
+            last_page = self.target_url
+            while (bounce_count < self.bounces):
+                delay = self.page_delay - random.randint(0,(self.page_delay * self.page_delay_jitter))
+                print(delay)
+                time.sleep(delay)
+                bounce_request = analytics_request(document_location=self.bounce_urls[random.randint(0,9)],document_referrer=last_page,client_id=self.client_id)
+                bounce_request.send()
+                pages.append(bounce_request.document_location)
+                last_page = bounce_request.document_location
+                bounce_count += 1
+            loop_count += 1
+        behavior = ''
+        for page in pages:
+            behavior = behavior + page + ' => '
 
-    def run(self):
-        target_request = analytics_request(document_location=self.target_url,document_referrer=self.referral_url,client_id=self.client_id)
-        target_request.send()
-        bounce_count = 0
-        last_page = self.target_url
-        while (bounce_count < self.bounces):
-            time.sleep(self.page_delay)
-            bounce_request = analytics_request(document_location=self.bounce_urls[random.randint(0,9)],document_referrer=last_page,client_id=self.client_id)
-            bounce_request.send()
-            last_page = bounce_request.document_location
-            bounce_count += 1
+        return behavior[:-4]
 
 
 
